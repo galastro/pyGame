65011de7005f1b23e1a9ede6cd2962b739439488
Leandro Galastro <lcgalastro@gmail.com>
2024-04-03 01:40:26 -0300

    paralax adicionado

--- MountainShooter/code/Entity.py
+++ MountainShooter/code/Entity.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
+from abc import ABC, abstractmethod
 
-class Entity:
-    def __init__(self):
+import pygame
+
+
+class Entity(ABC):
+    def __init__(self, name: str, position: tuple):
         self.name = None
-        self.surf = None
-        self.rect = None
+        self.surf = pygame.image.load('./assets/' + name + '.png')
+        self.rect = self.surf.get_rect(left=position[0], top=position[1])
+        self.speed = 0
 
+    @abstractmethod
     def move(self, ):
         pass
 
