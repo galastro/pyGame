bdc6d980ba0814c01df9ae9ec1e6fb280dbeb402
Leandro Galastro <lcgalastro@gmail.com>
2024-04-16 13:05:58 -0300

    criacao de tiro e colisoes

--- MountainShooter/code/Const.py
+++ MountainShooter/code/Const.py
@@ -14,10 +14,36 @@ ENTITY_SPEED = {'Level1Bg0': 0,
                 'Level1Bg5': 5,
                 'Level1Bg6': 6,
                 'Player1': 3,
+                'Player1Shot': 2,
                 'Player2': 3,
-                'Enemy1': 2,
-                'Enemy2': 1
+                'Player2Shot': 3,
+                'Enemy1': 1,
+                'Enemy1Shot': 4,
+                'Enemy2': 1,
+                'Enemy2Shot': 2
                 }
+
+ENTITY_HEALTH = {'Level1Bg0': 999,
+                 'Level1Bg1': 999,
+                 'Level1Bg2': 999,
+                 'Level1Bg3': 999,
+                 'Level1Bg4': 999,
+                 'Level1Bg5': 999,
+                 'Level1Bg6': 999,
+                 'Player1': 300,
+                 'Player1Shot': 1,
+                 'Player2Shot': 1,
+                 'Player2': 300,
+                 'Enemy1': 200,
+                 'Enemy1Shot': 1,
+                 'Enemy2': 200,
+                 'Enemy2Shot': 1,
+                 }
+ENTITY_SHOT_DELAY = {'Player1': 20,
+                     'Player2': 15,
+                     'Enemy1': 100,
+                     'Enemy2': 200
+                     }
 
 EVENT_ENEMY = pygame.USEREVENT + 1
 # M
@@ -38,6 +64,9 @@ PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
 
 PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
                     'Player2': pygame.K_d}
+
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
+                    'Player2': pygame.K_LCTRL}
 # W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
